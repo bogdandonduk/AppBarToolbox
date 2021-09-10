582ce870730d187678c9303288001f93f9a38e4b
bogdandonduk <bogdan.donduk@gmail.com>
2021-09-11 02:19:52 +0700

    Commit

---
+++ AppBarToolboxAndroidLib/src/main/java/bogdandonduk/appbartoolboxandroidlib/core/OnDrawerSlideBehaviour.kt
@@ -0,0 +1,16 @@
+package bogdandonduk.appbartoolboxandroidlib.core
+
+object OnDrawerSlideBehaviour {
+    sealed class MainContentOffset {
+        companion object {
+            const val DEFAULT_MAIN_CONTENT_PARTIAL_OFFSET_X_MULTIPLIER = 0.1f
+            const val DEFAULT_MAIN_CONTENT_PARTIAL_OFFSET_Y_MULTIPLIER = 0.1f
+        }
+
+        class MainContentFullOffset
+
+        class MainContentPartialOffset(var xMultiplier: Float = DEFAULT_MAIN_CONTENT_PARTIAL_OFFSET_X_MULTIPLIER, var yMultiplier: Float = DEFAULT_MAIN_CONTENT_PARTIAL_OFFSET_Y_MULTIPLIER)
+
+        class MainContentNoOffset
+    }
+}
