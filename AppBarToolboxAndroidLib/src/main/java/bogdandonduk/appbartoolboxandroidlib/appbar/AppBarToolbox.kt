f701be210aabdea33708a38879eb093e5de8f06e
bogdandonduk <bogdan.donduk@gmail.com>
2021-09-11 20:35:45 +0700

    Updated gradle plugin version

--- AppBarToolboxAndroidLib/src/main/java/bogdandonduk/appbartoolboxandroidlib/appbar/AppBarToolbox.kt
+++ AppBarToolboxAndroidLib/src/main/java/bogdandonduk/appbartoolboxandroidlib/appbar/AppBarToolbox.kt
@@ -5,7 +5,6 @@ import android.widget.ImageButton
 import androidx.appcompat.widget.ActionMenuView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.children
-import kotlinx.coroutines.delay
 
 object AppBarToolbox {
     fun getHomeAsUpIndicatorAsView(toolbar: Toolbar) : View? =
@@ -33,26 +32,26 @@ object AppBarToolbox {
             null
         } else null
 
-    suspend fun getOptionsMenuItemAsViewAsync(menuItemIdString: String, toolbar: Toolbar) : View? {
-        var view: View? = null
-
-        val timeout = System.currentTimeMillis() + 3000
-
-        do {
-            if(toolbar.childCount > 0) {
-                toolbar.children.forEach {
-                    if(it is ActionMenuView)
-                        it.children.forEach { menuItemChild ->
-                            if(menuItemChild.toString().contains(menuItemIdString, true)) view = menuItemChild
-                        }
-                }
-            }
-
-            if(view == null) delay(10)
-        } while(view == null && System.currentTimeMillis() < timeout)
-
-        return view
-    }
+//    suspend fun getOptionsMenuItemAsViewAsync(menuItemIdString: String, toolbar: Toolbar) : View? {
+//        var view: View? = null
+//
+//        val timeout = System.currentTimeMillis() + 3000
+//
+//        do {
+//            if(toolbar.childCount > 0) {
+//                toolbar.children.forEach {
+//                    if(it is ActionMenuView)
+//                        it.children.forEach { menuItemChild ->
+//                            if(menuItemChild.toString().contains(menuItemIdString, true)) view = menuItemChild
+//                        }
+//                }
+//            }
+//
+//            if(view == null) delay(10)
+//        } while(view == null && System.currentTimeMillis() < timeout)
+//
+//        return view
+//    }
 
     fun getOptionsMenuItemAsView(menuItemIdString: String, toolbar: Toolbar) : View? =
         if(toolbar.childCount > 0) {
@@ -67,11 +66,11 @@ object AppBarToolbox {
             null
         } else null
 
-    suspend fun manageOptionsMenuItemAsView(menuItemIdString: String, toolbar: Toolbar, managementAction: (view: View?) -> Unit) : View? {
-        val view = getOptionsMenuItemAsViewAsync(menuItemIdString, toolbar)
-
-        managementAction.invoke(view)
-
-        return view
-    }
+//    suspend fun manageOptionsMenuItemAsView(menuItemIdString: String, toolbar: Toolbar, managementAction: (view: View?) -> Unit) : View? {
+//        val view = getOptionsMenuItemAsViewAsync(menuItemIdString, toolbar)
+//
+//        managementAction.invoke(view)
+//
+//        return view
+//    }
 }
